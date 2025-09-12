3ee4d9bacb6479bdd253b5aee2c3203e532edc1e
spatial-intelligence <philbartie@gmail.com>
2025-09-12 13:55:16 +0100

    Demo Python to read RSS feed

---
+++ demorss.py
@@ -0,0 +1,13 @@
+#!/opt/venv/bin/python3
+
+import feedparser
+from datetime import datetime
+
+rss_url = "http://feeds.bbci.co.uk/news/rss.xml"
+
+feed = feedparser.parse(rss_url)
+top_entry = feed.entries[0] if feed.entries else None
+title = top_entry.title if top_entry else "No entries"
+
+with open("cron_rss.log", "a") as f:
+    f.write(f"{datetime.now()}: {title}\n")
