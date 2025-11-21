93f110f9c13678e7ac7309be1cbbab7d5488ef5a
spatial-intelligence <philbartie@gmail.com>
2025-11-21 15:23:13 +0000

    remove windows line endings
    sed -i 's/\r$//' /home/user50/week1/demorss.py

--- demorss.py
+++ demorss.py
@@ -6,7 +6,7 @@ from datetime import datetime
 rss_url = "http://feeds.bbci.co.uk/news/rss.xml"
 
 feed = feedparser.parse(rss_url)
-top_entry = feed.entries[0] if feed.entries else None
+top_entry = feed.entries[2] if feed.entries else None
 title = top_entry.title if top_entry else "No entries"
 
 with open("cron_rss.log", "a") as f:
